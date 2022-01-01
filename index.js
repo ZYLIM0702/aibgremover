f6379b6c0754ee1cbc0be8d6831573d3bfdaaccb
ZYLIM0702 <76156324+ZYLIM0702@users.noreply.github.com>
2022-01-02 00:46:21 +0800

    FINAL

---
+++ index.js
@@ -0,0 +1,69 @@
+var myArray = ['55abd9a523296eeeb3f6e52d7a515670239cabd5', 'a18148efaa09b1e74f7c8d83c93e8c5948853aef','8aeed560d0f202f6e85f45dd170c609aa87d9792', 'b5cd341e8cf1460e7d38c92c406965a262ec7078', 'b82799bea2a66099d4f6714e69162f1197352dbc','25dc38f697776dede763399514f13f274571d614']; 
+var rand = myArray[(Math.random() * myArray.length) | 0]
+
+
+
+
+
+
+
+document
+  .querySelector("input[type=file]")
+  .addEventListener("change", async function () {
+    const resizedImage = await loadImage(this.files[0], {
+      // resize before sending to PhotoRoom for performance
+      maxWidth: 30000,
+      maxHeight: 30000,
+      canvas: true
+    });
+
+    resizedImage.image.toBlob(async function (inputBlob) {
+      const formData = new FormData();
+      formData.append("image_file", inputBlob);
+
+      const response = await fetch("https://sdk.photoroom.com/v1/segment", {
+        method: "POST",
+        headers: {
+          "x-api-key": rand
+        },
+        body: formData
+      });
+
+
+      // https://developers.google.com/web/ilt/pwa/working-with-the-fetch-api#example_fetching_images
+      var outputBlob = await response.blob();
+      var outputImage = document.createElement("img");
+      document.querySelector("#result").appendChild(outputImage);
+      outputImage.setAttribute('id', 'bunss');
+      outputImage.classList.add('content-image');
+      outputImage.src = URL.createObjectURL(outputBlob);
+      
+
+const reader = new FileReader();
+reader.readAsDataURL(outputBlob); 
+reader.onloadend = function() {
+  var base64data = reader.result; 
+  reader.src = base64data.toString();
+   saveImg.href = reader.src ;    
+}
+
+function clickdownload() {
+ 
+            document.getElementById('saveImg');
+	    Android.downloading();
+            
+        }
+
+
+
+var image = document.querySelector('img'); // Image you want to save
+var saveImg = document.createElement("a");
+saveImg.setAttribute('id', 'downss');     // New link we use to save it with
+saveImg.classList.add('buttonss');
+saveImg.style.width ="570px";    // Assign image src to our link target
+saveImg.download = "Ai Background removed.png";
+saveImg.innerHTML = "CLICK TO DOWNLOAD IMAGE";       // Set link text
+document.body.appendChild(saveImg);   
+document.getElementById("downss").onclick = function() {clickdownload()};
+});
+});
---
+++ script.js
@@ -0,0 +1,213 @@
+// File Upload
+// 
+function ekUpload(){
+  function Init() {
+
+    var fileSelect    = document.getElementById('file-upload'),
+        fileDrag      = document.getElementById('file-drag'),
+        submitButton  = document.getElementById('submit-button');
+
+    fileSelect.addEventListener('change', fileSelectHandler, false);
+
+    // Is XHR2 available?
+    var xhr = new XMLHttpRequest();
+    if (xhr.upload) {
+      // File Drop
+      fileDrag.addEventListener('dragover', fileDragHover, false);
+      fileDrag.addEventListener('dragleave', fileDragHover, false);
+      fileDrag.addEventListener('drop', fileSelectHandler, false);
+    }
+  }
+
+  function fileDragHover(e) {
+    var fileDrag = document.getElementById('file-drag');
+
+    e.stopPropagation();
+    e.preventDefault();
+
+    fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload');
+  }
+
+  function fileSelectHandler(e) {
+    // Fetch FileList object
+    var files = e.target.files || e.dataTransfer.files;
+
+    // Cancel event and hover styling
+    fileDragHover(e);
+
+    // Process all File objects
+    for (var i = 0, f; f = files[i]; i++) {
+      parseFile(f);
+      uploadFile(f);
+    }
+  }
+
+  // Output
+  function output(msg) {
+    // Response
+    var m = document.getElementById('messages');
+    m.innerHTML = msg;
+  }
+
+  function parseFile(file) {
+
+    
+    output(
+      '<strong>' + encodeURI(file.name) + '</strong>'
+    );
+    
+    // var fileType = file.type;
+    // console.log(fileType);
+    var imageName = file.name;
+
+    var isGood = (/\.(?=gif|jpg|png|jpeg)/gi).test(imageName);
+    if (isGood) {
+      document.getElementById('start').classList.add("hidden");
+      document.getElementById('response').classList.remove("hidden");
+      document.getElementById('notimage').classList.add("hidden");
+      // Thumbnail Preview
+      document.getElementById('file-image').classList.remove("hidden");
+      document.getElementById('file-image').src = URL.createObjectURL(file);
+      document.getElementById('after').src = document.getElementById('file-image').src
+      document.getElementById('after').style.maxHeight="500px";
+      document.getElementById('after').style.maxWidth="500px";
+
+
+
+
+
+    }
+    else {
+      document.getElementById('file-image').classList.add("hidden");
+      document.getElementById('notimage').classList.remove("hidden");
+      document.getElementById('start').classList.remove("hidden");
+      document.getElementById('response').classList.add("hidden");
+      document.getElementById("file-upload-form").reset();
+    }
+  }
+
+
+
+  function uploadFile(file) {
+
+    var xhr = new XMLHttpRequest(),
+      fileInput = document.getElementById('file-upload'),
+      pBar = document.getElementById('file-progress'),
+      fileSizeLimit = 1000; // In MB
+    if (xhr.upload) {
+      // Check if file is less than x MB
+      if (file.size <= fileSizeLimit * 10000 * 10000) {
+        // Progress bar
+        
+
+        // File received / failed
+        xhr.onreadystatechange = function(e) {
+          if (xhr.readyState == 4) {
+            // Everything is good!
+
+            // progress.className = (xhr.status == 200 ? "success" : "failure");
+            // document.location.reload(true);
+          }
+        };
+
+        // Start upload
+        xhr.open('POST', document.getElementById('file-upload-form').action, true);
+        xhr.setRequestHeader('X-File-Name', file.name);
+        xhr.setRequestHeader('X-File-Size', file.size);
+        xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+        xhr.send(file);
+      } else {
+        output('Please upload a smaller file (< ' + fileSizeLimit + ' MB).');
+      }
+    }
+  }
+
+  // Check for the various File API support.
+  if (window.File && window.FileList && window.FileReader) {
+    Init();
+  } else {
+    document.getElementById('file-drag').style.display = 'none';
+  }
+}
+ekUpload();
+
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 30);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+// I hope this over-commenting helps. Let's do this!
+// Let's use the 'active' variable to let us know when we're using it
+let active = false;
+
+// First we'll have to set up our event listeners
+// We want to watch for clicks on our scroller
+document.querySelector('.scroller').addEventListener('mousedown',function(){
+  active = true;
+  // Add our scrolling class so the scroller has full opacity while active
+  document.querySelector('.scroller').classList.add('scrolling');
+});
+// We also want to watch the body for changes to the state,
+// like moving around and releasing the click
+// so let's set up our event listeners
+document.body.addEventListener('mouseup',function(){
+  active = true;
+  document.querySelector('.scroller').classList.remove('scrolling');
+});
+document.body.addEventListener('mouseout',function(){
+  active = true;
+  document.querySelector('.scroller').classList.remove('scrolling');
+});
+
+// Let's figure out where their mouse is at
+document.body.addEventListener('mousemove',function(e){
+  if (!active) return;
+  // Their mouse is here...
+  let x = e.pageX;
+  // but we want it relative to our wrapper
+  x -= document.querySelector('.wrapper').getBoundingClientRect().left;
+  // Okay let's change our state
+  scrollIt(x);
+});
+
+// Let's use this function
+function scrollIt(x){
+    let transform = Math.max(0,(Math.min(x,document.querySelector('.wrapper').offsetWidth)));
+    document.querySelector('.after').style.width = transform+"px";
+    document.querySelector('.scroller').style.left = transform-25+"px";
+}
+
+// Let's set our opening state based off the width, 
+// we want to show a bit of both images so the user can see what's going on
+scrollIt(150);
+
+// And finally let's repeat the process for touch events
+// first our middle scroller...
+document.querySelector('.scroller').addEventListener('touchstart',function(){
+  active = true;
+  document.querySelector('.scroller').classList.add('scrolling');
+});
+document.querySelector('.scroller').addEventListener('touchend',function(){
+  active = true;
+  document.querySelector('.scroller').classList.add('scrolling');
+});
+document.querySelector('.scroller').addEventListener('touchmove',function(){
+  active = true;
+  document.querySelector('.scroller').classList.add('scrolling');
+
+});
